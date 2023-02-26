541bb9fdc6e77b9293d51b8dcc1ea7cae1fe21d2
sucredoux <sakharovaes@yandex.ru>
2023-02-26 11:26:56 +0100

    Corrected navtab button

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -2,15 +2,10 @@ import "./NavTab.css";
 
 function NavTab() {
 
-    const location = "location.href";
-   function redirect() {
-    location ='#project';
-   }
-
     return (
         <div className="navtab">
             <div className="button navtab__button">
-                <a href="#project" className="navtab__button-link link">
+                <a href="#project" className="link navtab__button-link">
                     Узнать больше
                 </a>
             </div>
@@ -19,10 +14,3 @@ function NavTab() {
 };
 
 export default NavTab;
-
-               /* <a href="#project" className="navtab__button-link link">
-                    Узнать больше
-                </a>
-                onClick="location.href='#project'"
-                
-                */
